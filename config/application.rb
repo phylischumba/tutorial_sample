734d9124f716ce431fd2cc0f72a37a5654fd85bd
Phylis Chepchumba <chumba.phyl@gmail.com>
2019-12-10 14:06:44 +0300

    Add user following

--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,5 @@
 require_relative 'boot'
+require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
@@ -15,5 +16,6 @@ module TutorialSample
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.action_view.embed_authenticity_token_in_remote_forms = true
   end
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -6,6 +6,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
 		@user       = users(:michael)
 		@other_user = users(:archer)
 	end
+  
 	test "should redirect index when not logged in" do
 		get users_path
 		assert_redirected_to login_url
@@ -60,6 +61,16 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
       delete user_path(@user)
     end
     assert_redirected_to root_url
+  end
+
+  test "should redirect following when not logged in" do
+    get following_user_path(@user)
+    assert_redirected_to login_url
+  end
+
+  test "should redirect followers when not logged in" do
+    get followers_user_path(@user)
+    assert_redirected_to login_url
   end
 
 end
